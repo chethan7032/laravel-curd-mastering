41d5390dd79b6f207480794266a8d2d2341791e0
chethan7032 <chethanreddy118@gmail.com>
2025-09-04 17:46:01 +0530

    added customer controller and linked customer create page and index page

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',function(){
     return view('customers.index');
 
-});
+})->name("home");
+
+
+Route::resource('customers',CustomerController::class);
